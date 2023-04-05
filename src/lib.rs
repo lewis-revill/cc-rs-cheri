554a7106c15cc3625ebc7f5be283a23e5e7db994
Lewis Revill <lewis.revill@embecosm.com>
2023-04-05 11:43:40 +0100

    CHERI: Make sure CC target flags for Morello triples are set correctly
    We can't be adding 'morello-*' as a --target flag to Clang when the Rust
    triple starts with morello- as this is not recognised by the backend.

--- src/lib.rs
+++ src/lib.rs
@@ -67,6 +67,8 @@ use std::process::{Child, Command, Stdio};
 use std::sync::{Arc, Mutex};
 use std::thread::{self, JoinHandle};
 
+use home::home_dir;
+
 // These modules are all glue to support reading the MSVC version from
 // the registry and from COM interfaces
 #[cfg(windows)]
@@ -165,7 +167,7 @@ impl From<io::Error> for Error {
 }
 
 impl Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{:?}: {}", self.kind, self.message)
     }
 }
@@ -1455,7 +1457,21 @@ impl Build {
                 if !(target.contains("android")
                     && android_clang_compiler_uses_target_arg_internally(&cmd.path))
                 {
-                    if target.contains("darwin") {
+                    if target.starts_with("morello") {
+                        if target.contains("linux") {
+                            cmd.args.push("--target=aarch64-unknown-linux-musl_purecap".into());
+                            let sysroot = match home_dir() {
+                                Some(path) => path.as_path().join("morello").join("musl"),
+                                None => Path::new("").to_path_buf(),
+                            };
+                            let sysroot = sysroot.into_os_string().into_string().unwrap();
+                            cmd.args.push(format!("--sysroot={}", sysroot).into());
+                        } else {
+                            cmd.args.push("--target=aarch64-none-elf".into());
+                        }
+                        cmd.args.push("-march=morello+c64".into());
+                        cmd.args.push("-mabi=purecap".into());
+                    } else if target.contains("darwin") {
                         if let Some(arch) =
                             map_darwin_target_from_rust_to_compiler_architecture(target)
                         {
